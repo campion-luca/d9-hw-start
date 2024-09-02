389fa125a4702400c0b87988f2d1f09ffb54e20f
campion-luca <campionluca3@gmail.com>
2024-09-02 16:25:08 +0200

    first bug solved

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import store from './redux/store'
+import { Provider } from 'react-redux'
 
-root.render(<App />)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+<Provider store={store}>
+<App />
+</Provider>
+)
