882128a911c884d4db9989da34c31525dafe6461
campion-luca <campionluca3@gmail.com>
2024-09-02 15:24:39 +0200

    saving option add

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,20 +1,37 @@
-import { Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-const Job = ({ data }) => (
-  <Row
-    className="mx-0 mt-3 p-3"
-    style={{ border: '1px solid #00000033', borderRadius: 4 }}
-  >
-    <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-    </Col>
-    <Col xs={9}>
-      <a href={data.url} target="_blank" rel="noreferrer">
-        {data.title}
-      </a>
-    </Col>
-  </Row>
-)
+const Job = ({ data }) => {
+  const dispatch = useDispatch();
 
-export default Job
+  return (
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: "1px solid #00000033", borderRadius: 50 }}
+    >
+      <Col xs={2}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={8}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+        <Col xs={2}>
+          <Button
+            onClick={() => {
+              dispatch({
+                type: "ADD_TO_FAVOURITE",
+                payload: data,
+              });
+            }}
+          >
+            X
+          </Button>
+        </Col>
+      </Col>
+    </Row>
+  );
+};
+
+export default Job;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,23 @@
 const initialState = {
-    content: []
+    cart: {
+    favourite: []
+    }
 }
 
 const mainReducer = (state = initialState, action) => {
 
     switch (action.type) {
 
+        case "ADD_TO_FAVOURITE":
+            return{
+                ...state,
+                cart: {
+                    ...state.cart,
+                    favourite: [...state.cart.content, action.payload],
+                }
+            }
+
+            
     default: {
         return state
     }
